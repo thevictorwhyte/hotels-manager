3823900c9222ae8d67a5eac6aca0d88e6f975ded
Victor Whyte <victordavidwhyte@gmail.com>
2023-03-01 16:14:46 +0100

    feat: start on main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,62 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
 
 const Main = () => {
-
   return (
-    <Box flex={2} p={2}>Main</Box>
-  )
-}
+    <Box p={2} sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={6}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              {/* <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          /> */}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="caption">Nigeria</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  No. 23 Boullizard Avenua opposite first avenue, Abuja
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              {/* <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          /> */}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="caption">Nigeria</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  No. 23 Boullizard Avenua opposite first avenue, Abuja
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
 export default Main;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,45 @@
-import { Box } from '@mui/material';
-import React from 'react'
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+} from '@mui/material';
+import { useTheme } from '@mui/system';
+import HotelIcon from '@mui/icons-material/Hotel';
+import CategoryIcon from '@mui/icons-material/Category';
 
 const Sidebar = () => {
-
+  const theme = useTheme();
   return (
-    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block'} }}>Sidebar</Box>
-  )
-}
+    <Box
+      // bgcolor={theme.palette}
+      flex={1}
+      p={2}
+      sx={{ display: { xs: 'none', sm: 'block' } }}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HotelIcon />
+            </ListItemIcon>
+            <ListItemText primary="Hotels" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CategoryIcon />
+            </ListItemIcon>
+            <ListItemText primary="Categories" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
 
 export default Sidebar;
