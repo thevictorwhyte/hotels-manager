678a2089ebf73ec4473dcc17305848981ab05066
Victor Whyte <victordavidwhyte@gmail.com>
2023-03-01 15:52:03 +0100

    feat: complete navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import { AppBar, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  styled,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
   return (
     <AppBar position="static">
-      <Toolbar>Hotels Manager</Toolbar>
+      <StyledToolbar>
+        <Typography>Hotels Manager</Typography>
+        <Avatar onClick={() => setOpen(true)} />
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        open={open}
+        aria-labelledby="demo-positioned-button"
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
-}
+};
 
 export default Navbar;
